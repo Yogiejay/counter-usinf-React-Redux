1a1d514ec5bf785fbf60b4852b44aeac31e414f0
Yogie Yadav <yogiejay21@gmail.com>
2024-01-24 15:12:57 +0530

    Created counter using react reducer

--- counter-rtk/src/App.js
+++ counter-rtk/src/App.js
@@ -1,23 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useReducer } from "react";
+import counterReducer from "./CounterReducer";
 function App() {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App Using React Reducer</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+        }}
+      >
+        INCREMENT
+      </button>
+      <h1>{state.count}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "DECREMENT" });
+        }}
+      >
+        DECREMENT
+      </button>
     </div>
   );
 }
